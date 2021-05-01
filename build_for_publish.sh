9310e19daa497ba9dc015bc7b3256b3ff45f9e38
ShaneCreedon <shane.creedon3@mail.dcu.ie>
2021-05-01 16:08:14 +0100

    master: Increase npm version
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

---
+++ build_for_publish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./node_modules/.bin/webpack --config webpack.config.lib.prod.js -p --env=production
+cp ./.npmignore dist
+cp ./LICENSE dist
+cp ./package.json dist
+cp ./README.md dist
+rm -rf dist/demo
+mv ./dist/index.js ./dist/src
